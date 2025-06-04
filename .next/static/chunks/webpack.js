bf9479cd6122826bc17458b8a4d44b802cd9a1fc
Gauth72 <gfrere88@gmail.com>
2025-06-04 20:45:54 +0200

    fix: remove escape character in listings route

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "adfdcabed62e14ae"; }
+/******/ 		__webpack_require__.h = function() { return "b829cc431a79415f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
